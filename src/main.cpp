bb770dfe5c2824e8f67886edf5003090307d6670
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-07-24 08:39:23 -0400

    added node ID and refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,40 @@
 
 using namespace geode::prelude;
 
+long long epoch = std::chrono::system_clock::now().time_since_epoch().count();
+
 #define TITLE "Woah! Hold on there!"
 #define BODY "Are you sure you want to press this?"
-
-class QuickPopup : FLAlertLayer {}; // dummy class because i hate demangling class names manually
+#define TITLE_NODE_ID "title"
+#define TEXTAREA_NDID "content-text-area"
+#define TITLE_NDCHILD mainLayer->getChildByID(TITLE_NODE_ID)
+#define TEXTAREA_CHLD mainLayer->getChildByID(TEXTAREA_NDID)
 
 bool isButtonFromOwnMod(CCNode *node) {
-	// shut the fuck up CLion and clang-tidy, this is NOT recursive
+	// shut the fuck up CLion and clang-tidy, this is NOT recursive [anymore!]
 	if (!node || !node->getParent()) return false;
+	if (!utils::string::startsWith(node->getParent()->getID(), "confirmation-popup-"_spr)) return isButtonFromOwnMod(node->getParent());
 	if (typeinfo_cast<CCScene*>(node->getParent())) return false;
-	if (const auto popup = typeinfo_cast<QuickPopup*>(node->getParent())) {
-		CCLayer* mainLayer = typeinfo_cast<FLAlertLayer*>(popup)->m_mainLayer;
-		const std::string& title = mainLayer->getChildByType<CCLabelBMFont>(0)->getString();
-		if (const std::string& body = typeinfo_cast<CCLabelBMFont*>(mainLayer->getChildByType<TextArea>(0)->m_label->m_lines->objectAtIndex(0))->getString(); title == TITLE && body == BODY) return true;
-		return false;
-	}
-	return isButtonFromOwnMod(node->getParent());
+	CCLayer* mainLayer = static_cast<FLAlertLayer*>(node->getParent())->m_mainLayer;
+	const TextArea* textArea = TEXTAREA_CHLD ? static_cast<TextArea*>(TEXTAREA_CHLD) : mainLayer->getChildByType<TextArea>(0);
+	if (!textArea || !textArea->m_label || !textArea->m_label->m_lines || textArea->m_label->m_lines->count() < 1 || !textArea->m_label->m_lines->objectAtIndex(0)) return false;
+	CCLabelBMFont* titleLabel = TITLE_NDCHILD ? static_cast<CCLabelBMFont*>(TITLE_NDCHILD) : mainLayer->getChildByType<CCLabelBMFont>(0);
+	const auto firstLine = static_cast<CCLabelBMFont*>(textArea->m_label->m_lines->objectAtIndex(0));
+	const std::string& titleString = titleLabel->getString();
+	if (const std::string& firstString = firstLine->getString(); titleString == TITLE && firstString == BODY) return true;
+	return false;
 }
 
 class $modify(MyCCMenuItemSpriteExtra, CCMenuItemSpriteExtra) {
 	void activate() {
 		if (!this->m_bEnabled) return;
 		if (isButtonFromOwnMod(this)) return MyCCMenuItemSpriteExtra::runAsUsual();
-		geode::createQuickPopup(TITLE, BODY, "No", "Yes", [this](auto, const bool isButtonTwo) {
+		epoch = std::chrono::system_clock::now().time_since_epoch().count();
+		FLAlertLayer* popup = geode::createQuickPopup(TITLE, BODY, "No", "Yes", 420.f, [this](auto, const bool isButtonTwo) {
 			if (isButtonTwo) MyCCMenuItemSpriteExtra::runAsUsual();
-		});
+		}, false, false);
+		popup->setID(fmt::format("confirmation-popup-{}"_spr, epoch));
+		popup->show();
 	}
 	void runAsUsual() {
 		if (m_pListener && m_pfnSelector) (m_pListener->*m_pfnSelector)(this);
