8f4018d7d6ae8e597d176fcb771342e5717937d1
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-07-24 09:05:39 -0400

    move

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ long long epoch = std::chrono::system_clock::now().time_since_epoch().count();
 bool isButtonFromOwnMod(CCNode *node) {
 	// shut the fuck up CLion and clang-tidy, this is NOT recursive [anymore!]
 	if (!node || !node->getParent()) return false;
-	if (!utils::string::startsWith(node->getParent()->getID(), "confirmation-popup-"_spr)) return isButtonFromOwnMod(node->getParent());
 	if (typeinfo_cast<CCScene*>(node->getParent())) return false;
+	if (!utils::string::startsWith(node->getParent()->getID(), "confirmation-popup-"_spr)) return isButtonFromOwnMod(node->getParent());
 	CCLayer* mainLayer = static_cast<FLAlertLayer*>(node->getParent())->m_mainLayer;
 	const TextArea* textArea = TEXTAREA_CHLD ? static_cast<TextArea*>(TEXTAREA_CHLD) : mainLayer->getChildByType<TextArea>(0);
 	if (!textArea || !textArea->m_label || !textArea->m_label->m_lines || textArea->m_label->m_lines->count() < 1 || !textArea->m_label->m_lines->objectAtIndex(0)) return false;
